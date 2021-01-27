694e6d50c3322aa8aa0003b0fa364c35e7bcf792
jayashree-pixel <jaii.venu@gmail.com>
2021-01-27 16:55:59 +0530

    Added form to capture data and implemented button functions

--- todolist/urls.py
+++ todolist/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
 
-    path('', views.index, name='index')
-]
+    path('', views.index, name='index'),
+    path('add', views.addTodoItem, name='add'),
+    path('completed1/<todo_id>', views.completedTodo, name='completed'),
+    path('deletecompleted', views.deleteComplted, name='deletecompleted'),
+    path('deleteall', views.deleteAll, name='deleteall')
+    ]
